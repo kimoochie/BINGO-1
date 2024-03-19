68f3476edb9a4883695508baa2e3353410d59ab0
kimoochie <kk.ventura.wup@gmail.com>
2024-03-19 14:40:59 +0800

    third commit

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log("Hello World!");
+
+generateNumber(75);
+
+// function ; max is parameter
+function generateNumber(max) {
+    //Addition = + 
+    //Subtracion = -
+    //Multiplication = *
+    //Division = /
+    //Remainder = %
+    let randomNumber = Math.floor(Math.random() * max) + 1;
+
+    // if ...else if ...else
+    //GT >, GTE >= , LT <, LTE <= , EQ ==, NEQ != , AND &&, OR ||
+
+    if (randomNumber <= 15) {
+        console.log('The number is in B.');
+    } else if (randomNumber >= 16 && randomNumber <= 30) {
+        console.log('The number is in I.');
+    } else if (randomNumber >= 31 && randomNumber <= 45) {
+        console.log('The number is in N.');
+    } else if (randomNumber >= 46 && randomNumber <= 60) {
+        console.log('The number is in G.');
+    } else if (randomNumber >= 61 && randomNumber <= 75) {
+        console.log('The number is in O.');
+    } else  {
+        console.log('The number is invalid');
+    }
+    return console.log(randomNumber); 
+}
